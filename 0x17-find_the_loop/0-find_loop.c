74fc6297ca31386a91285eaf9d367f5786232d40
ElAmirr <othmaniamir41@gmail.com>
2022-06-02 20:43:18 +0100

    second

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -2,15 +2,27 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *current;
-	current = head;
-	if (head == NULL)
-		return (NULL);
-	while (current)
-	{
-		if (current->next >= current && current->next != NULL)
-			return (current->next);
-	current = current->next;
+listint_t *nextNode;
+listint_t *prev;
+nextNode = head;
+prev = head;
+while (head && nextNode && nextNode->next){
+	head = head->next;
+	nextNode = nextNode->next->next;
+	if (head == nextNode){
+		head = prev;
+		prev =  nextNode;
+		while (1){
+			nextNode = prev;
+			while (nextNode->next != head && nextNode->next != prev){
+				nextNode = nextNode->next;
+			}
+			if (nextNode->next == head)
+			break;
+			head = head->next;
+		}
+	return (nextNode->next);
 	}
-	return (NULL);
+}
+return (NULL);
 }
