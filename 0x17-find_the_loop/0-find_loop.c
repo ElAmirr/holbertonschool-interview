b2528240489bd0d07e8b2b91fd2f8274b6d47ad5
ElAmirr <othmaniamir41@gmail.com>
2022-06-02 20:47:52 +0100

    second

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -10,23 +10,27 @@ listint_t *nextNode;
 listint_t *prev;
 nextNode = head;
 prev = head;
-while (head && nextNode && nextNode->next){
-	head = head->next;
-	nextNode = nextNode->next->next;
-	if (head == nextNode){
-		head = prev;
-		prev =  nextNode;
-		while (1){
-			nextNode = prev;
-			while (nextNode->next != head && nextNode->next != prev){
-				nextNode = nextNode->next;
-			}
-			if (nextNode->next == head)
-			break;
-			head = head->next;
-		}
-	return (nextNode->next);
-	}
+while (head && nextNode && nextNode->next)
+{
+head = head->next;
+nextNode = nextNode->next->next;
+if (head == nextNode)
+{
+head = prev;
+prev =  nextNode;
+while (1)
+{
+nextNode = prev;
+while (nextNode->next != head && nextNode->next != prev)
+{
+nextNode = nextNode->next;
+}
+if (nextNode->next == head)
+break;
+head = head->next;
+}
+return (nextNode->next);
+}
 }
 return (NULL);
 }
